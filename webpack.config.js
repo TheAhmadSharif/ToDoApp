c08c66e7a82023a8a11eb4e94056554684683a98
Ahmad <ahmadshorif@gmail.com>
2017-01-02 18:00:31 +0600

    React boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 module.exports = {
-    entry: './public/app.jsx',
+    entry: './app/app.jsx',
     output: {
     		pathname: __dirname,
         filename: './public/bundle.js'    
      },
     resolve: {
+    	root: __dirname,
+    	alias: {
+    		Greeter: 'public/components/Greeter.jsx',
+    		GreeterMessage: 'public/components/GreeterMessage.jsx',
+    		GreeterForm: 'public/components/GreeterForm.jsx'
+    	},
       extensions:  ['', '.js', '.jsx']
     },
     module: {
-    	loaders: {
-    		loader: 'babel-loader',
-    		query: {
-    			presets: ['react': 'es2015']
-    		},
-    		test: 
-    	}
+    	loaders: [
+    		{
+    			loader: 'babel-loader',
+	    		query: {
+	    			presets: ['react','es2015']
+	    		},
+	    		test: /.\jsx?$/,
+	    		exclude: /(node_modules|bower_components)/
+    		}
+    	]
     }
-}
+};
