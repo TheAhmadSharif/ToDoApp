072300e88aa4667e4cbab94325e71ab46236bef2
Ahmad <ahmadshorif@gmail.com>
2017-01-04 01:23:51 +0600

    nested components

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,51 @@
+var GreeterForm = React.createClass({
+	clickAction: function (e) {
+		e.preventDefault();
+		var name = this.refs.name.value;
+		this.props.newName(name);
+		console.log('Hi', name);
+	},
+	render: function () {
+		return (
+			<form onSubmit={this.clickAction}>
+				<input type="text" ref="name" />
+				<button> Set Name </button>
+			</form>
+		);
+	}
+});
+
+
 var Greeter = React.createClass({
 	getDefaultProps: function () {
 		return {
-			name: 'Abul'
+			name: 'Ahmad'
 		}
+		
+	},
+	getInitialState: function () {
+		return {
+			name: this.props.name
+		}
+		
+	},
+	handleNewName: function (name) {
+		this.setState({
+				name: name
+			});
 	},
 	render: function () {
-	var name = this.props.name;
+		var name = this.state.name;
 		return (
-			<div> 
+			<div>
 				<h2> Hello {name} </h2>
+				<GreeterForm newName={this.handleNewName}/>
 			</div>
 		)
 	}
-})
-
+});
 
 ReactDOM.render(
- 	<Greeter/>,
+	<Greeter />,
 	document.getElementById('app')
 );
